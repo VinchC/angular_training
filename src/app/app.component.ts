487dd4e292a30ba755ca1c9a5011711c8e797b7e
Vincent C <vinch.cm@gmail.com>
2025-01-09 10:55:25 +0100

    PlayingCard component created and implemented in App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [PlayingCardComponent],
 })
 export class AppComponent {}
