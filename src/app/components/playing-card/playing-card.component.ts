0655b5b6f27fd75cd53c80717bfe594c9a447289
Vincent C <vinch.cm@gmail.com>
2025-01-13 10:10:13 +0100

    data sent via parent component directly

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-playing-card',
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './playing-card.component.css',
 })
 export class PlayingCardComponent {
-  name: string = 'My Monster';
-  hp: number = 40;
-  figureCaption: string = 'N° 001 Monster';
-  attackName: string = 'Geo Impact';
-  attackStrength: number = 60;
-  attackDescription: string =
-    "This is a long description and I don't know what to add - is it long enough ?";
+  @Input() name: string | undefined;
+  @Input() hp: number | undefined;
+  @Input() picture: string | undefined;
+  @Input() figureCaption: string | undefined;
+  @Input() attackName: string | undefined;
+  @Input() attackStrength: number | undefined;
+  @Input() attackDescription: string | undefined;
 }
