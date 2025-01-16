0b7e94e45b2323d6d8e7758f0dface990550264d
Vincent C <vinch.cm@gmail.com>
2025-01-16 16:49:24 +0100

    Update search value with value displayed implemented

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,14 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+
+  @Input() search = ""
   @Output() searchButtonClicked = new EventEmitter();
+  @Output() searchChange = new EventEmitter<string>();
+
+  updateSearch(value: string) {
+    this.searchChange.emit(value);
+  }
 
   searchClick() {
     this.searchButtonClicked.emit();
